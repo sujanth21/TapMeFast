1f5cf56ae38f0ad486db6a12ac2d8f3b7a8cc9b4
Sujanth <sujanth1987@gmail.com>
2017-08-19 09:21:17 +1000

    game completed

--- TapMeFast/ViewController.swift
+++ TapMeFast/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     var timer = Timer()
     
-    var timeInt = 0
+    var timeInt = 10
     var scoreInt = 0
     var gameInt = 0
     
@@ -32,6 +32,52 @@ class ViewController: UIViewController {
 
     @IBAction func startBtnPressed(_ sender: Any) {
         
+        if timeInt == 10 {
+            
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startCounter), userInfo: nil, repeats: true)
+            
+            startBtn.isEnabled = false
+            startBtn.alpha = 0.5
+        }
+        
+        if gameInt == 1 {
+            scoreInt += 1
+            scoreLabel.text = String(scoreInt)
+        } else {
+            timeInt = 10
+            scoreInt = 0
+            timeLabel.text = String(timeInt)
+            scoreLabel.text = String(scoreInt)
+            startBtn.setTitle("Start", for: .normal)
+        }
+        
+        
+    }
+    
+    @objc func startCounter() {
+        timeInt -= 1
+        timeLabel.text = String(timeInt)
+        
+        gameInt = 1
+        startBtn.isEnabled = true
+        startBtn.alpha = 1
+        
+        startBtn.setTitle("Tap Me", for: .normal)
+        
+        if timeInt == 0 {
+            timer.invalidate()
+            startBtn.isEnabled = false
+            startBtn.alpha = 0.5
+            
+            Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(restartGame), userInfo: nil, repeats: false)
+            startBtn.setTitle("Restart", for: .normal)
+        }
+    }
+    
+    @objc func restartGame() {
+        gameInt = 0
+        startBtn.isEnabled = true
+        startBtn.alpha = 1
     }
     
 }
